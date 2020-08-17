c40785cceba52f897f3615440dd3879bc9e4be88
onatm <onatmercan@gmail.com>
2020-08-17 22:43:29 +0100

    Do not execute from the command prompt

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,18 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const command = 'open-in-new-window.open';
+	const openCommand = 'open-in-new-window.open';
 
-	let commandHandler = vscode.commands.registerCommand(command, (uri: vscode.Uri) => {
+	let openCommandHandler = vscode.commands.registerCommand(openCommand, (uri?: vscode.Uri) => {
+		if (!uri) {
+			vscode.window.showWarningMessage('The intended usage is from the explorer context menu.');
+			return;
+		}
 		vscode.window.showInformationMessage(`Opening ${uri.path} in new window!`);
 		vscode.commands.executeCommand('vscode.openFolder', uri, true);
 	});
 
-	context.subscriptions.push(commandHandler);
+	context.subscriptions.push(openCommandHandler);
 }
 
 export function deactivate() { }
